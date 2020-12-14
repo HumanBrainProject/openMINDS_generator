7199d0f8c93646fcfa828e3d5150027b1ac7c619
Stefan Köhnen <s.koehnen@fz-juelich.de>
2020-12-14 13:45:38 +0100

    Add verify function to check arguments

--- openMINDS/validator.py
+++ openMINDS/validator.py
@@ -5,5 +5,11 @@ import jsonschema
 def main():
     print("Validator")
 
+def verify_parameters(argv):
+    if len(argv) != 2:
+        print("Wrong number of arguments")
+        help()
+        sys.exit(1)
+
 if __name__ == "__main__":
     main()
