3de9a01d414610a28c7c9872ded3d35feea0919e
Stefan Köhnen <s.koehnen@fz-juelich.de>
2020-12-14 13:47:22 +0100

    Add print for help

--- openMINDS/validator.py
+++ openMINDS/validator.py
@@ -5,11 +5,16 @@ import jsonschema
 def main():
     print("Validator")
 
+def print_help():
+    print("Usage:")
+    print("validator <json-ld-file>")
+
 def verify_parameters(argv):
     if len(argv) != 2:
         print("Wrong number of arguments")
-        help()
+        print_help()
         sys.exit(1)
 
 if __name__ == "__main__":
+    verify_parameters(sys.argv)
     main()
