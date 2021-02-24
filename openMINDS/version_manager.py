41ebd2ea4631f0ae4f364f3be25e0e498652a8cd
Stefan Köhnen <s.koehnen@fz-juelich.de>
2021-02-24 15:34:18 +0100

    Add download of v1

--- openMINDS/version_manager.py
+++ openMINDS/version_manager.py
@@ -1,10 +1,32 @@
 import os
 import git
+import urllib3
 import tempfile
-from pathlib import Path
+import pathlib
+import zipfile
 
 class Version_Manager:
 
     def __init__(self):
-        home = str(Path.home())
+        home = str(pathlib.Path.home())
         self.cache_dir = home + "/.openMINDS"
+        pathlib.Path(self.cache_dir).mkdir(parents=True, exist_ok=True)
+
+        self.versions = {
+            "dev": "https://github.com/HumanBrainProject/openMINDS/suites/2113925983/artifacts/43039730",
+            "v1.0.0": "https://github.com/HumanBrainProject/openMINDS/releases/download/v1.0.0/openMINDS_v1.0.0.zip"
+        }
+
+        http = urllib3.PoolManager()
+        response = http.request('GET', self.versions["v1.0.0"])
+        datatowrite = response.data
+
+        print("Download start")
+
+        v1_folder = self.cache_dir + "/v1.0.0/"
+        pathlib.Path(v1_folder).mkdir(parents=True, exist_ok=True)
+
+        with open(v1_folder + "v1.zip", 'wb+') as f:
+            f.write(datatowrite)
+
+        print("Download success")
