b09e91b5570bd8fbb057ef9e2d7a0bc6f841590d
Stefan Köhnen <s.koehnen@fz-juelich.de>
2022-12-21 16:47:37 +0100

    SQUASHED - First version of python generator
    This is the first version of moving the python generator from fairgraph
    into the openMINDS CI/CD pipeline.
    The generated python files are put into target/python/openminds

---
+++ generator/OpenMINDS_Base.py
@@ -0,0 +1,5 @@
+import json
+
+class OpenMINDS_Base:
+    def save(self):
+        
---
+++ generator/generate_python.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import shutil
+import argparse
+
+
+def main(schema_path, generator_path=None, ignore=[], build_docs=False):
+    #if generator_path is None:
+    #    generator_path = os.path.join(schema_path, "generator")
+    sys.path.append(os.path.expanduser(".."))
+
+    from commons import TARGET_PATH, OPENMINDS_VOCAB
+    from expander import Expander
+    from vocab_extractor import VocabExtractor
+    from PythonGenerator import PythonGenerator
+
+    print("Expanding the schemas...")
+    expander = Expander(schema_path, OPENMINDS_VOCAB, ignore=ignore)
+    expander.expand()
+    print("Extracting the vocab...")
+    types_file, properties_file = VocabExtractor(
+        expander.schemas, schema_path, reinit=True, current_version="v3",
+        vocab=OPENMINDS_VOCAB).extract()
+    expander.enrich_with_vocab(types_file, properties_file)
+
+    print("Clear target directory")
+    if os.path.exists(TARGET_PATH):
+        shutil.rmtree(TARGET_PATH)
+
+    if build_docs:
+        from generator.instance_locator import InstanceLocator
+        from generator.generate_html import HTMLGenerator
+        instances = InstanceLocator(schema_path).find_instances()
+        print("Generating HTML documentation...")
+        HTMLGenerator(expander.schemas, instances, current="v3", all_tags=[], all_version_branches=["v3"]).generate(ignore=[])
+
+    print("Generating Python classes...")
+    PythonGenerator(expander.schemas).generate(ignore=ignore)
+
+    shutil.copy("./OpenMINDS_Base.py", str(os.path.join(TARGET_PATH, "python", "openminds")))
+    shutil.copy("./fields.py", str(os.path.join(TARGET_PATH, "python", "openminds")))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog=sys.argv[0], 
+        description='Generate classes from the EBRAINS openMINDS schema templates'
+    )
+    parser.add_argument('schema_path', help="The path to the openMINDS directory")
+    parser.add_argument('--generator-path', help="The path to the openMINDS_generator directory")
+    parser.add_argument('--build-docs', help="Build HTML documentation", default=False, action='store_true')
+    parser.add_argument('--ignore', help="Names of schema groups to ignore", default=[], action='append')
+    args = vars(parser.parse_args())
+    main(**args)
